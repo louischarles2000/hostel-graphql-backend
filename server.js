4ad7aadff9b9c165fa64f465d930ebb550020289
unknown <louischarles20000>
2022-04-07 09:31:24 +0300

    initial

--- server.js
+++ server.js
@@ -1,27 +1,21 @@
 const dotenv = require('dotenv');
 dotenv.config('./.env');
-const bodyParser = require('body-parser');
 const { ApolloServer } = require('apollo-server');
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const userRoutes = require('./routes/userRoutes');
 const typeDefs = require('./apollo/typeDefs');
 const resolvers = require('./apollo/resolvers');
 const mongoose = require('mongoose');
-
-const PORT = process.env.port || 4000 ;
 
 const server = new ApolloServer({ typeDefs, resolvers, cors: {
     origin: 'http://localhost:3000',
     credentials: true
 } });
 
+const PORT = process.env.port || 4000;
 
 mongoose.connect('mongodb+srv://campuseyeadmin1:NGVIbHaCU2QEy72j@cluster0.vav4m.mongodb.net/hostels', { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
     console.log('Mongo DB connected')
-    return server.listen(PORT);
+    return server.listen({ port: PORT })
 })
 .then(({ url }) => {
     console.log(`Server ready at ${url} 🚀`);
